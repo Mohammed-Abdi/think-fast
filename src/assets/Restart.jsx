3451018e7515db35a1bc0ce0cf7f330c0b3660cf
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-07 21:54:22 +0300

    feat: add restart button on summary

---
+++ src/assets/Restart.jsx
@@ -0,0 +1,18 @@
+export default function Restart(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        d="M20 8c-1.403-2.96-4.463-5-8-5a9 9 0 1 0 0 18a9 9 0 0 0 9-9m0-9v6h-6"
+      ></path>
+    </svg>
+  );
+}
--- src/layout/Summary.jsx
+++ src/layout/Summary.jsx
@@ -1,5 +1,10 @@
+import { useContext } from "react";
+import { QuestionContext } from "../context/QuestionContext";
+import Restart from "../assets/Restart";
+
 const wrapperStyle = {
   display: "flex",
+  flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
   width: "100%",
@@ -7,9 +12,14 @@ const wrapperStyle = {
 };
 
 function Summary() {
+  const { dispatch } = useContext(QuestionContext);
   return (
     <div style={wrapperStyle}>
       <p>This is the Summary you got X/X</p>
+      <button className="cta-btn" onClick={() => dispatch({ type: "isReady" })}>
+        <Restart />
+        Restart Quiz
+      </button>
     </div>
   );
 }
