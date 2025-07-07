c8f7eb56b635c803927e58f96801ea49f0e85289
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-07 14:12:58 +0300

    feat: add Header component with styling

---
+++ src/layout/Header.jsx
@@ -0,0 +1,14 @@
+const headerStyle = {
+  fontSize: "3rem",
+  fontWeight: 600,
+  textAlign: "center",
+  padding: "1.25rem",
+  backgroundColor: "var(--dark-clr)",
+  color: "var(--light-clr)",
+};
+
+function Header() {
+  return <header style={headerStyle}>Think Fast</header>;
+}
+
+export default Header;
