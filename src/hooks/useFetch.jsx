f539c4f1806bb55634b06dac3c37306dc9bc5805
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-07 17:06:47 +0300

    perf: handle error possibilities

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,18 +2,23 @@ import { useEffect, useState } from "react";
 
 export function useFetch(url, dispatch) {
   const [data, setData] = useState();
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     async function getData() {
-      dispatch({ type: "isLoading" });
-      const res = await fetch(url);
-      if (!res.ok) throw new Error("couldn't fetch question");
-      dispatch({ type: "isReady" });
-      const data = await res.json();
-      setData(data);
+      try {
+        dispatch({ type: "isLoading" });
+        const res = await fetch(url);
+        if (!res.ok) throw new Error("couldn't fetch question");
+        dispatch({ type: "isReady" });
+        const data = await res.json();
+        setData(data);
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
     }
     getData();
   }, [url, dispatch]);
 
-  return { data };
+  return { data, errorMessage };
 }
